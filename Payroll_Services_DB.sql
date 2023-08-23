1baf7d71181b0a1eafcaef95d8c296ca1c7837b6
rahulkgautam <rahulkgautam125@gmail.com>
2023-08-23 22:42:43 +0530

    select all data into employee payroll with where condition

--- Payroll_Services_DB.sql
+++ Payroll_Services_DB.sql
@@ -1,1 +1,4 @@
-select * from Employee_Payroll
+select * from Employee_Payroll where EmployeeName='Bill'
+
+select * from Employee_Payroll WHERE StartDate BETWEEN CAST('2018-01-01'
+AS DATE) AND GETDATE();
